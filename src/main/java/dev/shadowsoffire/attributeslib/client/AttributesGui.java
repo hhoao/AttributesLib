5b6ba116ceb8ec8e6b3f849c1bd6ddfada686ed6
hhoao <haunghao@foxmail.com>
2026-04-25 08:35:55 +0800

    fix: let hide unchanged button receive clicks

--- src/main/java/dev/shadowsoffire/attributeslib/client/AttributesGui.java
+++ src/main/java/dev/shadowsoffire/attributeslib/client/AttributesGui.java
@@ -171,6 +171,8 @@ public class AttributesGui extends Screen {
     @Override
     public boolean isMouseOver(double pMouseX, double pMouseY) {
         if (!this.open) return false;
+        if (this.hideUnchangedBtn.visible && this.hideUnchangedBtn.isMouseOver(pMouseX, pMouseY))
+            return false;
         return this.isHovering(0, 0, WIDTH, 166, pMouseX, pMouseY);
     }
 
